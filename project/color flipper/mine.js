a9c9230a21eae8bf370753575e84192618128d11
Omid N Diba <omiddiba13@gmail.com>
2024-08-03 18:44:14 +0330

    up

--- project/color flipper/mine.js
+++ project/color flipper/mine.js
@@ -1,13 +1,13 @@
-const body = document.getElementsByTagName("body")[0];
-body.style.background = "#C0C0C0";
-const boxColorSet = document.getElementById("boxColorSet");
-function setColor(name) {
-  boxColorSet.style.background = name;
+document.body.style.backgroundColor = "#C0C0C0";
+
+function setColor(color) {
+  document.getElementById("boxColorSet").style.backgroundColor = color;
 }
+
 function randomColor() {
   const red = Math.round(Math.random() * 255);
   const green = Math.round(Math.random() * 255);
   const blue = Math.round(Math.random() * 255);
-  const color = `rgb(${red}, ${green},${blue})`;
-  boxColorSet.style.backgroundColor = color;
+  const color = `rgb(${red}, ${green}, ${blue})`;
+  document.getElementById("boxColorSet").style.backgroundColor = color;
 }
