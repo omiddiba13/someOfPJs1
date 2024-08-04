f54dc810c67c21151486638836055e166b1ac3c9
Omid N Diba <omiddiba13@gmail.com>
2024-08-04 23:40:47 +0330

    note

---
+++ note/main.js
@@ -0,0 +1,31 @@
+const noteContainer = document.getElementById('noteContainer');
+const createBtn = document.getElementById('btn');
+const note = document.getElementById('boxNote');
+
+function saveOnSystem() {
+	localStorage.setItem('notes', noteContainer.innerHTML);
+}
+function showNote() {
+	noteContainer.innerHTML = localStorage.getItem('notes');
+}
+createBtn.addEventListener('click', () => {
+	let inputbox = document.createElement('p');
+	let btnDelete = document.createElement('button');
+	inputbox.className = 'boxnote';
+	inputbox.setAttribute('contenteditable', 'true');
+	btnDelete.src = '';
+	noteContainer.appendChild(inputbox).appendChild(btnDelete);
+});
+noteContainer.addEventListener('click', function (e) {
+	if (e.target.tagName === 'BUTTON') {
+		e.target.parentElement.remove();
+		saveOnSystem();
+	} else if (e.target.tagName === 'P') {
+		notes = document.getElementById('boxNote');
+		notes.forEach((not) => {
+			not.onkeyup = function () {
+				saveOnSystem();
+			};
+		});
+	}
+});
