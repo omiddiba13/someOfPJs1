d0983a2d25803ea666664261cb1f1731f15550de
Omid N Diba <omiddiba13@gmail.com>
2024-08-06 13:40:23 +0330

    new up

--- age calculator/js.js
+++ age calculator/js.js
@@ -1,2 +1,23 @@
 let userInput = document.getElementById('date');
 userInput.max = new Date().toISOString().split('T')[0];
+
+function calculatorAge() {
+	let valueBirth = userInput.value;
+	let birthDate = new Date(valueBirth);
+	let today = new Date();
+	let age = today.getFullYear() - birthDate.getFullYear();
+	let monthDifference = today.getMonth() - birthDate.getMonth();
+	let ageNU = Number(age);
+	if (
+		monthDifference < 0 ||
+		(monthDifference === 0 && today.getDate() < birthDate.getDate())
+	) {
+		ageNU--;
+	}
+	document.getElementById('result').innerText = 'your age is' + ` : ${ageNU}`;
+	if (ageNU >= 18) {
+		alert('you can use a site or continue');
+	} else {
+		alert('nop');
+	}
+}
